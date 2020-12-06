ddcad1e6de658575d5f5c00e1cc2181b79fc6d2d
Nick Gerow <nick.gerow@enlightedinc.com>
2020-12-06 16:40:50 -0500

    Added part 2

--- Day 6/day6.py
+++ Day 6/day6.py
@@ -1,14 +1,21 @@
 def main() -> None:
     group = set()
+    intersection = None
     count = 0
+    intersection_count = 0
     with open("input", "r") as fin:
         for line in (l.strip() for l in fin):
             if line:
                 group.update(line)
+                answers = set(line)
+                intersection = answers if intersection is None else intersection & answers
             else:
                 count += len(group)
+                intersection_count += len(intersection)
                 group = set()
+                intersection = None
     print(count + len(group))
+    print(intersection_count + len(intersection))
 
 
 if __name__ == '__main__':
